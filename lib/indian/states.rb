133f80c945747b6f2216ced728e3a769c5e42c08
Pravinkumar Vaja <pravinkumar.vaja@synechron.com>
2014-04-03 14:55:03 +0530

    in_states method is now static method

--- lib/indian/states.rb
+++ lib/indian/states.rb
@@ -1,11 +1,10 @@
-#require "indian/states/version"
 require 'rubygems'
 require 'json'
 
 module Indian
   module States
 	STATES = JSON.parse(File.read('states.json'), :symbolize_names => true)
-	def in_states state=nil
+	def self.in_states state=nil
 		STATES
 	end
   end
--- lib/indian/states/version.rb
+++ lib/indian/states/version.rb
@@ -1,5 +1,5 @@
 module Indian
   module States
-    VERSION = "0.0.1"
+    VERSION = "0.0.3"
   end
 end
