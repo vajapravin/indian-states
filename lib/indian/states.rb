ebeb66f1420f200b3e6ee690b3c04453e54cd81f
vajapravin <vajapravin@ubuntu.ubuntu-domain>
2014-04-13 13:22:36 +0530

    states.json file not found error

--- lib/indian/states.rb
+++ lib/indian/states.rb
@@ -3,10 +3,9 @@ require 'json'
 
 module Indian
   module States
-	STATES = JSON.parse(File.read('states.json'), :symbolize_names => true)
+	STATES = JSON.parse(File.read("#{File.dirname(__FILE__)}/states.json"), :symbolize_names => true)
 	def self.in_states state=nil
 		STATES
 	end
   end
 end
-
